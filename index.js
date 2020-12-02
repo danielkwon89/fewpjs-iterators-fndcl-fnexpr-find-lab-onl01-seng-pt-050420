bc733aecc3072d6cbe432fea7290f30ec5c10cac
danielkwon89 <danielkwon89@gmail.com>
2020-12-01 23:31:45 -0500

    Done.

--- index.js
+++ index.js
@@ -4,12 +4,6 @@
 //   return "hi"
 // }
 
-// const record = [
-//   {year: "2018", result: "L"},
-//   {year: "2017", result: "W"},
-//   {year: "2016", result: "N/A"}
-// ]
-
 superbowlWin = (array) => {
   const winner = array.find(element => element.result === "W")
   return winner ? winner.year : winner
